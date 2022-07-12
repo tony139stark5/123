0eae292a49fe4bf22742177125f4e544c8aa1e9b
Vimukthi_Oshada <108446553+oshadaXD@users.noreply.github.com>
2022-07-12 17:50:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,32 +12,32 @@ global.APIKeys = {
 }
 
 // Other
-global.namabot = ['Oshada']
-global.namaowner = ['Oshada']
+global.namabot = ['⛧ S A T H A N ⸸ ']
+global.namaowner = ['⛧ S A T H A N ⛥']
 global.owner = ['94775792013']
 global.premium = ['94775792013']
 global.packname = 'Sticker By'
-global.author = 'O S H D A User BOT'
+global.author = '⛧ S A T H A N ⸸ '
 global.sessionName = 'zxyn'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: '✓ Success',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    admin: 'Group Admin Special Features!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'Owner Bot s Special Features',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'Bot Number User Special Features',
     wait: 'Loading...',
-    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
 global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.thumb = fs.readFileSync('./lib/zxyn.jpg')
-global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+global.thumb = fs.readFileSync('./lib/ab67616d0000b273e10e2e89ef9b6516991eb36c.jpg')
+global.visoka = { url: 'https://telegra.ph/file/39c0aba89c1281605b7de.mp4' }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
